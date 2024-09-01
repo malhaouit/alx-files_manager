d914d74a66d0ed508060585db6d6ee371990ece5
malaouit <abdelmounaim.abdel@gmail.com>
2024-09-01 05:48:51 -0700

    Implement a Redis Client

---
+++ utils/redis.js
@@ -0,0 +1,62 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.isConnected = false;
+
+    this.connectionPromise = new Promise((resolve) => {
+      this.client.on('connect', () => {
+        this.isConnected = true;
+        resolve(true);
+      });
+    });
+
+    this.client.on('error', (err) => console.error('Redis Client Error', err));
+  }
+
+  async isAlive() {
+    await this.connectionPromise;
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
