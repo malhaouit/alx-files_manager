9b4b4dd73b06b1d314c81c79d15585b6b766cd84
malaouit <abdelmounaim.abdel@gmail.com>
2024-09-01 05:51:26 -0700

    Implement a database client

---
+++ utils/db.js
@@ -0,0 +1,42 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.dbName = database;
+
+    this.db = null;
+    this.connected = false;
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(this.dbName);
+        this.connected = true;
+      })
+      .catch(() => {
+        this.connected = false;
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) throw new Error('MongoDB not connected');
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) throw new Error('MongoDB not connected');
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
