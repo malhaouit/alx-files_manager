baf10f7b45ef4bd43fb4dbb7760d27b01d2219cc
malaouit <abdelmounaim.abdel@gmail.com>
2024-09-02 13:42:20 -0700

    Fix lint style

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
 import routes from './routes/index';
+
+const express = require('express');
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use('/', routes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
