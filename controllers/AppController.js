11bd6a2e0728e6611dca840d1109730770dc002c
malaouit <abdelmounaim.abdel@gmail.com>
2024-09-02 14:01:58 -0700

    Implement the logic for handling requests to /status and /stats

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,20 @@
-class AppController {
-    static getStatus(req, res) {
-        res.status(200).json({ 
-            "redis": true,
-            "db": true
-         });
-    }
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-    static getStats(req, res) {
-        const stats = {
-            users: 12,
-            files: 1231
-        };
-        res.status(200).json(stats);
-    }
+class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    res.status(200).json({ users, files });
+  }
 }
 
 export default AppController;
